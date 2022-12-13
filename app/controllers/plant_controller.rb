68c01a020b030d0264bbe545869edaa98d8e4dc8
Diana Kwamboka <diana.kwamboka@student.moringaschool.com>
2022-12-13 14:36:15 +0300

    completed assignment

---
+++ app/controllers/plant_controller.rb
@@ -0,0 +1,26 @@
+class PlantController < ApplicationController
+    class PlantsController < ApplicationController
+        def index
+            plants = Plant.all
+            render json: plants
+        end
+          #GET /plants/:id
+        def show
+            plant = Plant.find_by(id: params[:id])
+            if plant
+                render json: plant
+            else
+                render json: { error: "Plant not found" }, status: :not_found
+            end
+        end
+        def create
+            plant = Plant.create(plant_params)
+            render json: plant, status: :created
+        end
+          private
+          # all methods below here are private
+        def plant_params
+            params.permit(:name, :image, :price)
+        end
+    end
+end
